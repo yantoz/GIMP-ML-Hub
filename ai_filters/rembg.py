68c42ba6627a7b529737d6c1e3b7f45bfc183ea4
yantoz <yantoz@users.noreply.github.com>
2023-12-02 07:59:20 +0900

    Added alpha matting options for rembg and sam.

--- ai_filters/rembg.py
+++ ai_filters/rembg.py
@@ -18,9 +18,12 @@ class RemBg(FilterBase):
         ]
 
         params = [
-            [Type.OPTION, "model",          "Model",          0,     self.model_list],
-            [Type.BOOL,   "mask",           "Generate Mask",  True                  ],
-            [Type.BOOL,   "force_cpu",      "Force CPU",      False                 ],
+            [Type.OPTION, "model",          "Model",              0,                 self.model_list],
+            [Type.BOOL,   "mask",           "Generate Mask",      True                              ],
+            [Type.BOOL,   "alpha_matting",  "Alpha Matting",      False                             ],
+            [Type.RANGE,  "thresholds",     "  - Thresholds",     [10,240],          (0,255)        ],
+            [Type.SLIDER, "erode_size",     "  - Erosion Size",   10,                (0,255)        ],
+            [Type.BOOL,   "force_cpu",      "Force CPU",          False                             ],
         ]
 
         super().__init__(parent, title, name, info, params)
@@ -31,11 +34,27 @@ class RemBg(FilterBase):
         params = self.params
         model = params["model"]
         mask = params["mask"]
+        alpha_matting = params["alpha_matting"]
+        thresholds = params["thresholds"]
+        erode_size = params["erode_size"]
         self._force_cpu = params["force_cpu"]
+
+        """
+            alpha_matting (bool, optional): Flag indicating whether to use alpha matting. Defaults to False.
+            alpha_matting_foreground_threshold (int, optional): Foreground threshold for alpha matting. Defaults to 240.
+            alpha_matting_background_threshold (int, optional): Background threshold for alpha matting. Defaults to 10.
+            alpha_matting_erode_size (int, optional): Erosion size for alpha matting. Defaults to 10.
+        """
+        alpha_matting_background_threshold = int(thresholds[0])
+        alpha_matting_foreground_threshold = int(thresholds[1])
+        alpha_matting_erode_size = int(erode_size)
 
         self.model_file = "RemBg.py"
         model_name = self.model_list[model][1]
-        result = self.predict(img, mask, model=model_name)
+        result = self.predict(img, mask, model=model_name,
+            alpha_matting=alpha_matting, alpha_matting_foreground_threshold=alpha_matting_foreground_threshold,
+            alpha_matting_background_threshold=alpha_matting_background_threshold, alpha_matting_erode_size=alpha_matting_erode_size)
+
         if result:
             layer = self.create_layer(result, mask=mask, reposition=True)
             self.message()
--- ai_filters/sam.py
+++ ai_filters/sam.py
@@ -11,6 +11,9 @@ class SAM(FilterBase):
         params = [
             [Type.RADIO,  "type",           "Type",                      1,                 ("Point","Box")],
             [Type.BOOL,   "mask",           "Generate Mask",             True                              ],
+            [Type.BOOL,   "alpha_matting",  "Alpha Matting",             False                             ],
+            [Type.RANGE,  "thresholds",     "  - Thresholds",            [10,240],          (0,255)        ],
+            [Type.SLIDER, "erode_size",     "  - Erosion Size",          10,                (0,255)        ],
             [Type.BOOL,   "force_cpu",      "Force CPU",                 False                             ],
         ]
 
@@ -22,6 +25,9 @@ class SAM(FilterBase):
         params = self.params
         model = "sam"
         type = params["type"]
+        alpha_matting = params["alpha_matting"]
+        thresholds = params["thresholds"]
+        erode_size = params["erode_size"]
         mask = params["mask"]
         self._force_cpu = params["force_cpu"]
 
@@ -46,9 +52,22 @@ class SAM(FilterBase):
                 "type": "rectangle",
                 "data": [selection.x(),selection.y(),selection.x()+selection.width(),selection.y()+selection.height()],
             }]
+        print(sam_prompt)
+
+        """
+            alpha_matting (bool, optional): Flag indicating whether to use alpha matting. Defaults to False.
+            alpha_matting_foreground_threshold (int, optional): Foreground threshold for alpha matting. Defaults to 240.
+            alpha_matting_background_threshold (int, optional): Background threshold for alpha matting. Defaults to 10.
+            alpha_matting_erode_size (int, optional): Erosion size for alpha matting. Defaults to 10.
+        """
+        alpha_matting_background_threshold = int(thresholds[0])
+        alpha_matting_foreground_threshold = int(thresholds[1])
+        alpha_matting_erode_size = int(erode_size)
 
         self.model_file = "SAM.py"
-        result = self.predict(img, model="sam", sam_prompt=sam_prompt)
+        result = self.predict(img, model="sam", sam_prompt=sam_prompt,
+            alpha_matting=alpha_matting, alpha_matting_foreground_threshold=alpha_matting_foreground_threshold,
+            alpha_matting_background_threshold=alpha_matting_background_threshold, alpha_matting_erode_size=alpha_matting_erode_size)
 
         if result:
             doc.setSelection(None)
